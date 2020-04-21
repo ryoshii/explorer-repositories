cba252d1845c3fc90cd9a6eedb598f6ec6f5cf12
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-21 09:45:37 -0300

    Add style in dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +14,56 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do reposiório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/6109271?s=460&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+            alt="Rodrigo Yoshioka"
+          />
+          <div>
+            <strong>ryoshii/be-the-hero</strong>
+            <p>
+              Projeto desenvolvido na Semana Omnistack 11.0 ministrada pela
+              Rocketseat.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/6109271?s=460&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+            alt="Rodrigo Yoshioka"
+          />
+          <div>
+            <strong>ryoshii/be-the-hero</strong>
+            <p>
+              Projeto desenvolvido na Semana Omnistack 11.0 ministrada pela
+              Rocketseat.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/6109271?s=460&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+            alt="Rodrigo Yoshioka"
+          />
+          <div>
+            <strong>ryoshii/be-the-hero</strong>
+            <p>
+              Projeto desenvolvido na Semana Omnistack 11.0 ministrada pela
+              Rocketseat.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
